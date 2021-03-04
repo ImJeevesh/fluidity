1cfda7167a0d43328fe49a6b196e89a1a7b1e500
ImJeevesh <Jrawat2009@gmail.com>
2021-03-05 00:11:39 +0530

    feat(init): create base for fluidity

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/preset-typescript"
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+}
