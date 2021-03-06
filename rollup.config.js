f079884a9f573a6b20f02929f40f7548b46e3b2c
ImJeevesh <Jrawat2009@gmail.com>
2021-03-06 21:41:30 +0530

    chore(build): fix rollup to prepare build

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,42 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
+import babel from 'rollup-plugin-babel';
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
+import svg from "rollup-plugin-svg";
+import css from "rollup-plugin-import-css";
 
 const packageJson = require("./package.json");
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.tsx',
+  inlineDynamicImports: true,
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true
     },
     {
       file: packageJson.module,
-      format: "esm",
+      format: 'esm',
       sourcemap: true
     }
   ],
+  external: [
+    'react',
+    'react-dom'
+  ],
   plugins: [
+    css(),
+    svg(),
     peerDepsExternal(),
-    resolve(),
+    resolve({ extensions: ['.jsx', '.js', '.tsx'] }),
     commonjs(),
+    babel({
+      extensions: ['.jsx', '.js', '.tsx'], 
+      exclude: 'node_modules/**'
+    }),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss()
   ]
 };
