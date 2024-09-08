dc4ea6e5166ddb6592ff632b8f425a1ade6f4702
ImJeevesh <jrawat2009@gmail.com>
2024-09-08 16:26:56 +0530

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url';
 import { extname, relative, resolve } from 'path';
 import react from '@vitejs/plugin-react';
-import { glob } from 'glob';
+import glob from 'glob';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
